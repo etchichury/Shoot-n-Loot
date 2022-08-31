2e36da6b6ea519bb53d86eb98cfc9c930b3b4dc9
etchichury <joaoetchichury@hotmail.com>
2022-08-30 23:08:04 -0300

    Add shooting arrow functionality

--- shoot-n-loot/src/Player.gd
+++ shoot-n-loot/src/Player.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
 onready var animatedSprite = $AnimatedSprite
+onready var atack_timer = $AttackTimer
+
 export (int) var speed = 200
+export var dagger = preload("res://src/projectiles/PlayerArrow.tscn")
+
 var velocity = Vector2()
 
 func get_keyboard_input():
@@ -20,12 +24,28 @@ func get_keyboard_input():
 	
 	velocity = velocity.normalized() * speed
 
+func get_mouse_input():
+	if Input.is_action_pressed("action_fire"):
+		var dagger_direction = self.global_position.direction_to(get_global_mouse_position())
+		throw_dagger(dagger_direction)
+		atack_timer.start()
+
+# TODO: change 'dagger' term to 'arrow'
+func throw_dagger(dagger_direction: Vector2):
+	if Input.is_action_pressed("action_fire") && atack_timer.is_stopped():
+		var dagger_instance = dagger.instance()
+		get_tree().current_scene.add_child(dagger_instance)
+		dagger_instance.global_position = self.global_position
+		
+		var dagger_angle = dagger_direction.angle()
+		dagger_instance.rotation = dagger_angle
+
 func _physics_process(_delta):
-	var input = Vector2()
+	get_mouse_input()
 	
 	get_keyboard_input()
 	velocity = move_and_slide(velocity)
-	
+
 	if velocity == Vector2(0,0):
 		animatedSprite.set_animation("idle")
 	else:
